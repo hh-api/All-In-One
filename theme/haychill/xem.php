6b9e9a380f28f3b9d8be505650bfcdde1261cc6a
hh-api <131185043+hh-api@users.noreply.github.com>
2024-04-29 14:03:12 +0700

    Update xem.php

--- theme/haychill/xem.php
+++ theme/haychill/xem.php
@@ -1,5 +1,5 @@
 <?php
-include 'theme/'.$theme.'/includes/head.php';
+include 'theme/'.$theme.'/head.php';
 ?>
     <div id="body-wrap" class="container">
         <div id="content">
@@ -175,9 +175,9 @@ include 'theme/'.$theme.'/item.php';
             </div>
         </div>
 <?php
-include 'theme/'.$theme.'/includes/BXH.php';
+include 'theme/'.$theme.'/BXH.php';
 ?>
     </div>
 <?php
-include 'theme/'.$theme.'/includes/fot.php';
+include 'theme/'.$theme.'/fot.php';
 ?>
